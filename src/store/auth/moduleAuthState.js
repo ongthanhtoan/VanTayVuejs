519398684e0cae4e8a034009671e22e2e9376854
ongthanhtoan <toanlh3@gmail.com>
2020-03-25 21:38:02 +0700

    add source

---
+++ src/store/auth/moduleAuthState.js
@@ -0,0 +1,24 @@
+/*=========================================================================================
+  File Name: moduleAuthState.js
+  Description: Auth Module State
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+
+import auth from "@/auth/authService";
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export default {
+    isUserLoggedIn: () => {
+        let isAuthenticated = false
+
+        if (auth.isAuthenticated()){
+          isAuthenticated = true;
+        }else isAuthenticated = false
+        return (localStorage.getItem('userInfo') && isAuthenticated)
+    },
+}
